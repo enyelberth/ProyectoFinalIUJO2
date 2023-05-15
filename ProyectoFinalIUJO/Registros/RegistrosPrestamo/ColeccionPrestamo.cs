26271413941b6c92db011099b2dab23cb54621c2
Enyelberth Rodriguez <enyelberthrc22.z@gmail.com>
2023-05-15 18:47:23 -0400

    dasd
    asd

--- ProyectoFinalIUJO/Registros/RegistrosPrestamo/ColeccionPrestamo.cs
+++ ProyectoFinalIUJO/Registros/RegistrosPrestamo/ColeccionPrestamo.cs
@@ -4,8 +4,25 @@ using System.Linq;
 using System.Text;
 
 namespace ProyectoFinalIUJO.Registros.RegistrosPrestamo
-{
-    class ColeccionPrestamo
+{/*
+
+   Módulo de registro de Préstamos
+  * , en el que se lleva los datos concernientes al préstamo del DVD que va a llevar el cliente en un momento dado,
+  * y en el que se registra por cada película en DVD un monto de 2 $ por 3 días, cada juego en cónsola 2 $ por 4 días y 2 $ por 
+  * cada DVD de pistas de audio por una semana. dando un monto total que deberá pagar el cliente al momento del registro.
+
+Consulta de Préstamos, en los que pueden mostrarse en una tabla los artículos en préstamos por Cliente, planteando la fecha de 
+  * devolución del artículo, o bien los artículos a devolverse para una fecha dada.
+
+Consulta de Ingresos, en los que se muestra en una tabla los montos obtenidos por préstamos en una fecha dada, 
+  * colocada a conveniencia del usuario.
+
+  * 
+El programa debe validar la entrada de datos tanto en los DVD, las Clientes y los registros de Préstamos, sin abortar el programa y 
+  * emitiendo los debidos mensajes de error hacia el usuario. Además, debe plantearse una opción del programa en el que pueda respaldar y 
+  * restaurar datos en un archivo permanente.
+  */ 
+  class ColeccionPrestamo
     {
     }
 }
