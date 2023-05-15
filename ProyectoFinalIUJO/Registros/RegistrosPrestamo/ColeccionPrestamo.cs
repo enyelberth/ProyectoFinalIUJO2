7eb541461ae3dd173906f6982b639806d9344d17
Enyelberth Rodriguez <enyelberthrc22.z@gmail.com>
2023-05-15 18:35:37 -0400

    Carpetas de Prestamos
    carpetas de prestamos añadida

---
+++ ProyectoFinalIUJO/Registros/RegistrosPrestamo/ColeccionPrestamo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProyectoFinalIUJO.Registros.RegistrosPrestamo
+{
+    class ColeccionPrestamo
+    {
+    }
+}
