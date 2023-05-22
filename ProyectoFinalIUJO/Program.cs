af20a59422b73ff7385628921f0090139decb77b
Enyelberth Rodriguez <64881597+enyelberth@users.noreply.github.com>
2023-05-22 19:14:31 +0200

    Final
    asdasd

--- ProyectoFinalIUJO/Program.cs
+++ ProyectoFinalIUJO/Program.cs
@@ -20,7 +20,8 @@ namespace ProyectoFinalIUJO
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
+			//Application.Run(new MainForm());
+            Application.Run(new Login());
 			
 		}
 		
